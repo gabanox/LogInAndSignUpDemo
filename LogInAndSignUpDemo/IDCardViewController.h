7c3b292c82220ace7ba15450a2e8c652b84168d2
Gabriel <isc.gabriel.ramirezm@gmail.com>
2013-03-25 22:53:16 -0600

    View controller destinado para ser el gafete de presentación

---
+++ LogInAndSignUpDemo/IDCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDCardViewController.h
+//  LogInAndSignUpDemo
+//
+//  Created by Gabriel Ramirez on 3/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDCardViewController : UIViewController
+
+@end
